e5dc3263a377deb34a049230c0996c9e2349e6c2
lemoncat <xar4ak7even5@gmail.com>
2024-06-08 15:09:52 +0300

    Update asset path for SVG icons in app component

--- src/app/shared/interceptors/base-url/base-url.interceptor.ts
+++ src/app/shared/interceptors/base-url/base-url.interceptor.ts
@@ -3,7 +3,7 @@ import { environment } from '../../../../environments/environment.development';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const baseUrl = environment.apiUrl;
-  const blackList = ['/assets', 'http', 'https'];
+  const blackList = ['assets', 'http', 'https'];
 
   return blackList.some((url) => req.url.startsWith(url))
     ? next(req)
